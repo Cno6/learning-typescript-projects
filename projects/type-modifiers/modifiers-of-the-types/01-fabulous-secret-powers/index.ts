97574cee484522708270135e340c893c607a609d
Vadim Igumnov <vad.igumnoff@gmail.com>
2024-01-14 17:59:27 +0300

    learning session #4

--- projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
+++ projects/type-modifiers/modifiers-of-the-types/01-fabulous-secret-powers/index.ts
@@ -1,4 +1,19 @@
 import { characters } from "./characters";
 
+interface Character {
+	name: string;
+	powers: string[];
+	side: "good" | "evil";
+}
+
 // Write your announceCharacter function here! ✨
 // You'll need to export it so the tests can run it.
+export function announceCharacter(character: string) {
+	const char = JSON.parse(character) as Character;
+
+	console.log(`I am ${char.name}.`);
+	console.log(`My powers are: ${char.powers.join(", ")}.`);
+	console.log(`I am ${char.side}.`);
+
+	return char;
+}

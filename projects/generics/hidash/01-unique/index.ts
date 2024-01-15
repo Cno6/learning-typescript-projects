e57fa96697755d7d6d066f0481337770cdd8a4aa
@v.igumnov <v.igumnov@mashroom.live>
2024-01-15 17:08:24 +0300

    learning session #5

--- projects/generics/hidash/01-unique/index.ts
+++ projects/generics/hidash/01-unique/index.ts
@@ -1,2 +1,11 @@
 // Write your unique function here! ✨
 // You'll need to export it so the tests can run it.
+export function unique<T>(...items: T[][]) {
+	let result = new Set<T>();
+
+	items.forEach((item) => {
+		result = new Set([...result, ...item]);
+	});
+
+	return Array.from(result.values());
+}
--- projects/generics/hidash/02-zip/index.ts
+++ projects/generics/hidash/02-zip/index.ts
@@ -1,2 +1,13 @@
 // Write your zip function here! ✨
 // You'll need to export it so the tests can run it.
+export function zip<T, K>(a: T[], b: K[]) {
+	const longestLength = a.length > b.length ? a.length : b.length;
+	const result = [];
+
+	for (let i = 0; i < longestLength; i++) {
+		if (a[i]) result.push(a[i]);
+		if (b[i]) result.push(b[i]);
+	}
+
+	return result;
+}
